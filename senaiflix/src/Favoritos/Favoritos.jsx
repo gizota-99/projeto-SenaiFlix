3dd858312caa20858200ca582d83588ae060ba89
GIOVANNA TOSSATO RODRIGUES <47958210892@info.local>
2026-03-14 15:54:31 -0300

    Projeto pendente apens favoritos

---
+++ senaiflix/src/Favoritos/Favoritos.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Header from "../Header";
+import Footer from "../Footer";
+import { Container, MovieList, Movie } from "../style";
+
+function Favorites(){
+    const img_path = 'https://image.tmdb.org/t/p/w500/'
+}
+return(<>
+</>)
+
+export default Favorites
--- senaiflix/src/main.jsx
+++ senaiflix/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './global.css'
 import App from './App.jsx'
+import Details from './Details/Details.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/details/:type/:id' element={<Details/>}/>
+    </Routes>
     </BrowserRouter>
   </StrictMode>
 )
